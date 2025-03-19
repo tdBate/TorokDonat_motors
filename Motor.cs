4a466896e2bc5f8db73a56a5ca322a91c8ef0a12
Torok Donat <td.zazsa@gmail.com>
2025-03-19 16:35:54 +0100

    Statisztika osztály kész

--- Motor.cs
+++ Motor.cs
@@ -11,10 +11,10 @@ namespace Motors
         private string brand;
         private string name;
         private int releaseYear;
-        private int performance;
-        private int priceInEur;
+        private double performance;
+        private double priceInEur;
 
-        public Motor(string brand, string name, int releaseYear, int performance, int priceInEur)
+        public Motor(string brand, string name, int releaseYear, double performance, double priceInEur)
         {
             this.brand = brand;
             this.name = name;
@@ -26,8 +26,8 @@ namespace Motors
         public string Brand { get => brand;}
         public string Name { get => name;}
         public int ReleaseYear { get => releaseYear; }
-        public int Performance { get => performance; }
-        public int PriceInEur { get => priceInEur; }
+        public double Performance { get => performance; }
+        public double PriceInEur { get => priceInEur; }
 
 
 
---
+++ Statisztika.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motors
+{
+    internal class Statisztika
+    {
+        List<Motor> motors = new List<Motor>();
+
+        public void ReadFromFile(string fileName)
+        {
+            StreamReader sr = new StreamReader(fileName);
+            sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string[] tagok = line.Split(";");
+                motors.Add(new Motor(tagok[0], tagok[1], int.Parse(tagok[2]), double.Parse(tagok[3]), double.Parse(tagok[4])));
+            }
+        }
+
+        public int SumPrices()
+        {
+            int osszeg = 0;
+            for (int i = 0; i < motors.Count; i++)
+            {
+                osszeg += (int)motors[i].PriceInEur;
+            }
+            return osszeg;
+        }
+
+        public bool Contains(string motorName)
+        {
+            for (int i = 0; i < motors.Count; i++)
+            {
+                if (motors[i].Name.Equals(motorName)) { return true; }
+            }
+            return false;
+        }
+
+        public Motor Oldest()
+        {
+            Motor min = motors[0];
+            for (int i = 1; i < motors.Count; i++)
+            {
+                if (min.ReleaseYear > motors[i].ReleaseYear)
+                {
+                    min = motors[i];
+                }
+            }
+            return min;
+        }
+
+        public int SumBasedOnBrand(string brandName)
+        {
+            List<Motor> tempMotorok = motors;
+            
+            for (int i = 0; i < motors.Count; i++)
+            {
+                if (!(motors[i].Brand.Equals(brandName)))
+                {
+                    motors.RemoveAt(i);
+                }
+            }
+            int osszeg = SumPrices();
+
+            motors = tempMotorok;
+            return SumPrices();
+        }
+
+        public void Sort()
+        {
+            bool marade = true;
+            while (marade)
+            {
+                marade = false;
+                for (int i = 0; i < motors.Count-1; i++)
+                {
+                    if (motors[i].Performance < motors[i + 1].Performance)
+                    {
+                        marade = true;
+                        (motors[i], motors[i + 1]) = (motors[i + 1], motors[i]);
+                    }
+                }
+            }
+        }
+    }
+}
